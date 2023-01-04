a5a671553188fff6326538c155de0b90bd2bfee6
林鹏涛 <326661445@qq.com>
2023-01-04 20:06:59 +0800

    完成StudyCard组件封装

---
+++ src/Components/StudyCard/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {RightCircleOutlined } from '@ant-design/icons';
+import  s from "./style.module.scss"
+
+type Props={
+    itemName:string
+    itemContent:string
+    itemIcon:string,
+    itemUrl:string
+}
+const StudyCard = (props:Props) => {
+    const {itemName,itemContent,itemIcon,itemUrl} = props
+    return (
+        <div className={s.LinkItem}>
+            <a className={s.ItemBody} href={itemUrl} title={itemContent} target="_blank">
+                <img src={itemIcon} className="Item-Avatar" />
+                <div className={s.ItemTitle}>
+                    {itemName}
+                </div>
+                <div className={s.ItemContent}>
+                    {itemContent}
+                </div>
+                <div className={s.ItemArrow}>
+                    <RightCircleOutlined style={{color:"#999999",fontSize:"16px"}} />
+                </div>
+            </a>
+        </div>
+    )
+};
+
+export default StudyCard;
--- src/pages/Links/index.tsx
+++ src/pages/Links/index.tsx
@@ -5,8 +5,14 @@ import bgImage from "../../assets/links_banner.svg"
 const Links = () => {
     return (
         <>
-            <div>
-                <img src={bgImage} />
+            <div className={s.wrapper}>
+                <div className={s.img}>
+                    <img src={bgImage} alt=""/>
+                    <div className={s.font}>
+                        <p>温馨提示</p>
+                        <p>这里放置了一些学习导航网站，可供大家学习使用。</p>
+                    </div>
+                </div>
             </div>
         </>
     );
